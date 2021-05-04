5e161c6e402625544a4cda9e1496b005dcfe1499
Natalia Efimtseva <efimtceva@gmail.com>
2021-05-04 17:47:54 +0300

    Add database branch and files

---
+++ Tailspin.SpaceGame.Database/dbo/Tables/Achievements.sql
@@ -0,0 +1,5 @@
+CREATE TABLE [dbo].[Achievements] (
+    [id]            INT           NOT NULL,
+    [description]   NVARCHAR (50)    NULL,
+    PRIMARY KEY CLUSTERED ([id] ASC)
+);
---
+++ Tailspin.SpaceGame.Database/dbo/Tables/Profile.sql
@@ -0,0 +1,6 @@
+CREATE TABLE [dbo].[Profiles] (
+    [id]                INT           NOT NULL,
+    [userName]          NVARCHAR (50) NOT NULL,
+    [avatarUrl]         NVARCHAR (50) NULL,
+    PRIMARY KEY CLUSTERED ([id] ASC)
+);
